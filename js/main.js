2732daf8ef20423460234f067c45c345b5ea8ad6
Michael Lackey <lackey@Michaels-MBP.attlocal.net>
2020-02-01 14:46:07 -0600

    complete game logic

--- js/main.js
+++ js/main.js
@@ -1,75 +1,66 @@
-/*------Constants------*/
-
 /*------Variables (state)------*/
 let player;
-let winner;
 let board;
 let count = 1;
-
-// Variables might include (board/turn/winner)
+let winner = false;
 
 /*------Cached Element References------*/
 const playAgain = document.getElementById('play');
 const message = document.getElementById('message');
 
-// You might choose to put your game status here
-
 /*------Event Listeners------*/
-document.getElementById('board').addEventListener('click', click);
-playAgain.addEventListener('click', initialize);
-
-// This is where you should put the event listener
-// for a mouse-click
+document.querySelector('section.board').addEventListener('click', click);
 
 /*------Functions------*/
-initialize();
-
-function initialize() {
-    board = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
-
-
-    render();
+function init() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    player = 1;
+    message.textContent = "X's Go First!";
+    playAgain.style.visibility = 'hidden';
 }
 
-function checkWinner() {
-    if (board[0]+board[1]+board[2] === 3 ||
-        board[0]+board[3]+board[6] === 3 ||
-        board[0]+board[4]+board[8] === 3 ||
-        board[1]+board[4]+board[7] === 3 ||
-        board[2]+board[4]+board[6] === 3) {
-        board[2]+board[5]+board[8] === 3 ||  
-        board[3]+board[4]+board[5] === 3 ||
-        board[6]+board[7]+board[8] === 3 ||  
-        message.textContent = "X's win!";
-        winner = true;
-    }   
-    if (board[0]+board[1]+board[2] === -3 ||
-        board[0]+board[3]+board[6] === -3 ||
-        board[0]+board[4]+board[8] === -3 ||
-        board[1]+board[4]+board[7] === -3 ||
-        board[2]+board[4]+board[6] === -3) {
-        board[2]+board[5]+board[8] === -3 ||  
-        board[3]+board[4]+board[5] === -3 ||
-        board[6]+board[7]+board[8] === -3 ||  
-        message.textContent = "O's win!";
-        winner = true;
-    }  
+function gameOver() {
+    return ((count === 10 && winner === false) || winner === true);
 }
 
 function click(event) {
     let square = parseInt(event.target.id.replace('sq', ''));
-    if (board[square] !== 'null')
-        return;
+    if (board[square] !== '')
+    return;
     checkWinner();
-    playAgain.style.visibility = checkWinner() ? 'visible' : 'hidden';
     render(square);
 }
 
+function checkWinner() {
+    if (board[0]+board[1]+board[2] === 3 || 
+        board[0]+board[3]+board[6] === 3 || 
+        board[0]+board[4]+board[8] === 3 || 
+        board[1]+board[4]+board[7] === 3 || 
+        board[2]+board[4]+board[6] === 3 ||
+        board[2]+board[5]+board[8] === 3 || 
+        board[3]+board[4]+board[5] === 3 || 
+        board[6]+board[7]+board[8] === 3){
+        message.textContent = "X's win!";
+        winner = true;
+    }   
+    if (board[0]+board[1]+board[2] === -3 || 
+        board[0]+board[3]+board[6] === -3 || 
+        board[0]+board[4]+board[8] === -3 || 
+        board[1]+board[4]+board[7] === -3 || 
+        board[2]+board[4]+board[6] === -3 ||
+        board[2]+board[5]+board[8] === -3 || 
+        board[3]+board[4]+board[5] === -3 || 
+        board[6]+board[7]+board[8] === -3){
+        message.textContent = "O's win!";
+        winner = true;
+    }
+}
+    
 function render(square) {
     if (winner === false) {
         markSpot = document.getElementById(`sq${square}`);
-        board[square] = turn;
-        if (turn === 1) {
+        board[square] = player;
+        if (player === 1) {
             markSpot.textContent = "X";
             message.textContent = "O's Go!";
         } else {
@@ -79,31 +70,11 @@ function render(square) {
     }
     player *= -1;
     checkWinner();
-    count = count + 1;
-
-    if (count === 10 && getWinner === false) {
-        message.textContent = "Draw!";
+    count++;
+    if (count === 10 && winner === false) {
+    message.textContent = "Draw!";
     }
+    playAgain.style.visibility = gameOver() ? 'visible' : 'hidden';
 }
 
-// Some functions you might choose to use:
-
-// Initialization function:
-// Where you set your initial state, setting up 
-// what the board will look like upon loading
-
-// On-Click function:
-// Set up what happens when one of the elements
-// is clicked
-
-
-// Check winner function:
-// Checks the current state of the board for
-// a winner and changes the state of the winner
-// variable if so
-
-
-// Render function:
-// Displays the current state of the board
-// on the page, updating the elements to reflect
-// either X or O depending on whose turn it is
+init();

7a98f8e95433382c94f1ca2c4a2aa69e83737977
Michael Lackey <lackey@Michaels-MacBook-Pro.local>
2020-02-01 11:28:12 -0600

    set JS barebones

--- js/main.js
+++ js/main.js
@@ -3,21 +3,63 @@
 
 
 /*------Variables (state)------*/
-let player();
+let player;
+let winner;
+let message;
 
 // Variables might include (board/turn/winner)
 
 /*------Cached Element References------*/
+const sq0 = document.getElementById('sq0');
+const sq1 = document.getElementById('sq1');
+const sq2 = document.getElementById('sq2');
+const sq3 = document.getElementById('sq3');
+const sq4 = document.getElementById('sq4');
+const sq5 = document.getElementById('sq5');
+const sq6 = document.getElementById('sq6');
+const sq7 = document.getElementById('sq7');
+const sq8 = document.getElementById('sq8');
+const playAgain = document.getElementById('play');
 
 // You might choose to put your game status here
 
 /*------Event Listeners------*/
+document.getElementById('board').addEventListener('click', click);
+playAgain.addEventListener('click', initialize);
 
 // This is where you should put the event listener
 // for a mouse-click
 
 /*------Functions------*/
+initialize();
 
+function initialize() {
+
+
+
+
+    render();
+}
+
+function click() {
+
+
+}
+
+function render() {
+
+
+}
+
+function renderMessage() {
+
+
+}
+
+function checkWinner() {
+
+
+}
 
 // Some functions you might choose to use:
 
